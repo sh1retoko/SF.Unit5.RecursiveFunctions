c77c19e1ed30fed7320cf2969a26566c8fbb9054
sh1retoko <120920943+sh1retoko@users.noreply.github.com>
2023-02-21 12:01:28 +0300

    Add files via upload

---
+++ Рекурсивные функции/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рекурсивные_функции
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажите глубину эха");
+            var deep = int.Parse(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2); // Remove метод, который удаляет указанное число символов в строке
+            }
+
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+            {
+                Echo(modif, deep - 1);
+            }
+        }
+    }
+}
